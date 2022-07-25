03aca40529661f8d5dd8d6992fddc6b86a0981c2
facuariasla <facundolautaroarias@hotmail.com>
2022-07-25 11:10:36 -0300

    home cover modified

--- src/components/HomeCover.jsx
+++ src/components/HomeCover.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Stack, Heading } from "@chakra-ui/react";
+import { Stack, Heading, Text } from "@chakra-ui/react";
 import useStore from "../global_state";
 import { RiMoneyDollarCircleLine } from "react-icons/ri";
 
@@ -9,18 +9,22 @@ const HomeCover = () => {
 
   return (
     <Stack align="center">
-      <Heading>IR A BUY/SELL</Heading>
-      <Heading>IR A BUY/SELL</Heading>
       <Heading>IR A PAYMENT</Heading>
-      <Heading>IR A PAYMENT</Heading>
-      <Stack direction="row" align='center' spacing={0}>
+      <Text>Cargar falsos créditos (número tarjeta + 4200)</Text>
+      <hr/>
+      <Heading>IR A BUY/SELL</Heading>
+      <Text>Hacer compra de alguna crypto con los USD disponibles</Text>
+      <hr/>
+      <Stack direction="row" align="center" spacing={0}>
         <Heading>CREDITS: {realUSD ? realUSD : "0"} USD </Heading>
       </Stack>
       <Stack>
         <ul>
-          {tokensInAcc?.map((el)=>
-          <li key={Math.random()*99999}>{el.token}: {el.amount}</li>
-          )}
+          {tokensInAcc?.map((el) => (
+            <li key={Math.random() * 99999}>
+              {el.token}: {el.amount}
+            </li>
+          ))}
         </ul>
       </Stack>
     </Stack>
